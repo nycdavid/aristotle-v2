0fd8ea1f52addddb9836815db9e80311333cf011
David Ko <david.ko@damncarousel.com>
2014-11-30 15:15:36 -0500

    Closes issue #5. Guest can signup from landing page

---
+++ spec/features/guest_can_signup_from_landing_page_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+feature 'Guest can signup from landing page' do
+  scenario 'Guest navigates to root' do
+    visit root_path 
+    click_link 'Sign Up'
+
+    expect(page).to have_selector 'div[rel="signup"]'
+  end
+end
