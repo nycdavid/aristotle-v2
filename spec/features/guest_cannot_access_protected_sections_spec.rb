491a6904c98dece9d3dc237d45213c753ee58f3f
David Ko <david@jinglepunks.com>
2014-12-11 11:20:47 -0500

    Guest cannot access protected sections specs and implementation

--- spec/features/guest_cannot_access_protected_sections_spec.rb
+++ spec/features/guest_cannot_access_protected_sections_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature 'Guest cannot access the protected sections' do
   scenario 'when he is not signed in' do
     visit user_path
-    expect(page).to have_content '404'
+    expect(page).to have_selector '*[rel="404-not-found"]'
   end
 end
