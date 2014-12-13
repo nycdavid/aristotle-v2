4f491f1a30b3a8635c48af35e49e9b2ca30b44a8
David Ko <david.ko@damncarousel.com>
2014-12-13 12:55:21 -0500

    Copy resources

--- app/assets/javascripts/users/application.js
+++ app/assets/javascripts/users/application.js
@@ -1,6 +1,6 @@
 // These are all of the JavaScripts that pertain to the registered Users section
 //= require jquery
 //= require jquery_ujs
-//= require angular/angular
-//= require angular-resource/angular-resource.min
+//= require vendor/angular/angular.min
+//= require vendor/angular-resource/angular-resource.min
 //= require_tree .
