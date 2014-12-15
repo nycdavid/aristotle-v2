5ad05c4886c1e81dd84cac5ed7820c2af7b7c30f
David Ko <david.ko@damncarousel.com>
2014-12-15 09:35:12 -0500

    Guest sees proper links when he isn't signed in

--- spec/features/guest_gets_landing_page_spec.rb
+++ spec/features/guest_gets_landing_page_spec.rb
@@ -4,5 +4,7 @@ feature 'Guest gets landing page' do
   scenario 'Guest navigates to root' do
     visit root_path
     expect(page).to have_selector '*[rel="landing"]'
+    expect(page).to have_link 'Sign Up'
+    expect(page).to have_link 'Login'
   end
 end
