ebe6da3f8bee52f50a14cf6d71b66f32e82da4a7
David Ko <david.ko@damncarousel.com>
2014-12-16 07:53:24 -0500

    Remove title time directive

--- app/assets/javascripts/users/ng_pomodoro/directives/title_time.js
+++
@@ -1,8 +0,0 @@
-angular
-  .module('titleTimeDirective', [])
-  .directive('titleTime', function(timeHelper) {
-    return function(scope, element, attrs) {
-      var data = scope[attrs['titleTime']];  
-      element.text(timeHelper.timeRemainingString(scope));
-    };
-  });
