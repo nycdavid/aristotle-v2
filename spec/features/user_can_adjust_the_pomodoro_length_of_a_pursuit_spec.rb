79a81ec5a3d1ca74012dd3e992efff35904d33aa
David Ko <david.ko@damncarousel.com>
2015-01-02 14:29:40 -0500

    Add feature to edit pursuit from multiple entry points

--- spec/features/user_can_adjust_the_pomodoro_length_of_a_pursuit_spec.rb
+++ spec/features/user_can_adjust_the_pomodoro_length_of_a_pursuit_spec.rb
@@ -1,18 +1,32 @@
 require 'rails_helper'
 
-feature 'User can adjust the pomodoro length of a pursuit' do
+feature 'User can edit a pursuit' do
   before :each do
     @user = FactoryGirl.create :user
     @pursuit = FactoryGirl.create(:pursuit, { user_id: @user.id })
     page.set_rack_session(user_id: @user.id)
   end
 
-  scenario 'User navigates to pursuit edit page and selects a radio button to adjust time' do
+  scenario 'User edits default pomodoro length by navigating to edit page' do
     visit edit_user_pursuit_path @pursuit
     fill_in 'pursuit[pomodoro_length_in_minutes]', with: '15'
     click_button 'Save'
 
     expect(page).to have_selector '*[rel="success-flash"]'
     expect(find("tr[data-pursuit-id='#{@pursuit.id}']").find('.default-pomodoro-length').text).to eq('00:15:00')
+  end
+
+  scenario 'User can edit a pursuit by clicking edit from show page' do
+    visit user_pursuit_path(@pursuit.id)
+    click_link 'Edit'
+
+    expect(page).to have_content "Edit: #{@pursuit.name}"
+  end
+
+  scenario 'User can edit a pursuit by clicking edit from the PursuitsIndex page' do
+    visit user_pursuits_path
+    page.find("tr.pursuit[data-pursuit-id=\"#{@pursuit.id}\"]").find('td.actions').click_link('Edit')
+
+    expect(page).to have_content "Edit: #{@pursuit.name}"
   end
 end
