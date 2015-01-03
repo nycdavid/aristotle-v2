2b489b5467903376639ebab93888f9b6645014ea
David Ko <david.ko@damncarousel.com>
2015-01-03 13:06:17 -0500

    Inject rootscope dependency

--- app/assets/javascripts/users/ng_pomodoro/controllers/title_ctrl.js
+++ app/assets/javascripts/users/ng_pomodoro/controllers/title_ctrl.js
@@ -1,8 +1,8 @@
-function TitleCtrl($scope, $http) {
+function TitleCtrl($scope, $rootScope, $http) {
   $scope.time = '00:00';
   $rootScope.$on('timeUpdate', function(event, args) {
     $scope.time = args.formattedTimeRemaining;
   });
 };
-TitleCtrl.$inject = ['$scope', '$http'];
+TitleCtrl.$inject = ['$scope', '$http', '$rootScope'];
 AristotleApp.controller('TitleCtrl', TitleCtrl);
