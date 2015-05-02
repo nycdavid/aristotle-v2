39459851b90495b682129f867760a5d9cf2e32b1
David Ko <david.ko@damncarousel.com>
2015-05-02 14:26:49 -0400

    Removed extraneous test

--- spec/features/user/user_can_set_his_timezone_spec.rb
+++ spec/features/user/user_can_set_his_timezone_spec.rb
@@ -6,13 +6,6 @@ feature 'User can set his timezone' do
     page.set_rack_session(user_id: user.id)
   end
 
-  scenario 'User navigates to account settings' do
-    visit edit_user_path  
-    select('Eastern Time (US & Canada)', from: 'user[timezone]')
-    click_button 'Save'
-    expect(page).to have_content 'Region/Time Zone: Eastern Time (US & Canada)'
-  end
-
   scenario 'User clicks on settings link in side nav' do
     visit user_path
     click_link 'Edit Account'
