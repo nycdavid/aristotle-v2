a2cb8b024e87deebaf9201b449442d3fd9a7d30a
David Ko <david.ko@damncarousel.com>
2015-07-12 15:14:16 -0400

    Refactored stylesheets

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  layout "guest"
+
   def home
     redirect_to user_path if user_signed_in?
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   before_action :find_user, only: [:create]
+  layout "guest"
 
   def new
   end
