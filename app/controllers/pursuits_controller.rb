34b58c7c3d898d8a0c8a03c2e2902f555539286d
David Ko <david.ko@pvtmethod.com>
2016-12-04 07:02:10 -0500

    wip: Move sizing over to Bootstrap grid

--- app/controllers/pursuits_controller.rb
+++ app/controllers/pursuits_controller.rb
@@ -5,7 +5,7 @@ class PursuitsController < ApplicationController
 
   def index
     @pursuits = current_user.pursuits
-    @range = params[:range].nil? ? "overall" : params[:range]
+    @range = params[:range].nil? ? "today" : params[:range]
     @inspirational_quote = InspirationalQuote.random
     @section_heading = "Your Pursuits"
   end
@@ -17,7 +17,7 @@ class PursuitsController < ApplicationController
 
   def create
     @pursuit = current_user.pursuits.new(pursuit_params)
-    @pursuit.save ? allow_pursuit : refuse_pursuit 
+    @pursuit.save ? allow_pursuit : refuse_pursuit
   end
 
   def show
@@ -57,7 +57,7 @@ class PursuitsController < ApplicationController
       flash[:danger] = "We're sorry, something went wrong..."
       render :new
     end
-  
+
     def pursuit_params
       params.require(:pursuit).permit(:name, :pomodoro_length_in_minutes, :pomodoro_length_in_seconds)
     end
