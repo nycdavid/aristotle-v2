f1d38fecd66a99df9d4190af3333a4d5dec705b9
David Ko <david.ko@pvtmethod.com>
2016-12-09 16:26:47 -0500

    wip: push notifications

---
+++ app/assets/javascripts/users/push_notifications.js
@@ -0,0 +1,14 @@
+'use strict';
+
+$(document).ready(function() {
+  if ('serviceWorker' in navigator && 'PushManager' in window) {
+    navigator.serviceWorker.register('/service_worker.js')
+      .then(function(swReg) {
+        console.log('Service Worker is registered:', swReg);
+        swReg.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: new Uint8Array([])
+        });
+      })
+  }
+});
