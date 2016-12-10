1c9259d98b7516af351a0b9ca0790b9549332a06
David Ko <david.ko@pvtmethod.com>
2016-12-10 11:03:30 -0500

    Change key

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,6 @@ class PagesController < ApplicationController
   end
 
   def lets_encrypt
-    render text: "8M1uxnFhyG7ig6M73kthxpTa7KDvIbERscUWfXqorwI.YcyMFZ13uEF6X5xwbAarteuUWUfXkFrH9awQ1bCOf8k"
+    render text: "XXaqt4lDl1INllHOVN8o10fKnRuDLtLPopCns6DX-5o.YcyMFZ13uEF6X5xwbAarteuUWUfXkFrH9awQ1bCOf8k"
   end
 end
