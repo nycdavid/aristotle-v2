e7ab9fe4cfc32192be5a63f88043c6892712e559
David Ko <david.ko@pvtmethod.com>
2016-12-13 13:25:16 -0500

    Fix today display on calendars

--- spec/views/pursuits/show.html.haml_spec.rb
+++ spec/views/pursuits/show.html.haml_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "pursuits/show", type: :view do
   let(:pursuit) { FactoryGirl.create :pursuit, user: user }
 
   it "shows the correct date according to a User's timezone" do
-    utc = Time.local(2008, 9, 2, 1, 0, 0)
+    utc = Time.utc(2008, 9, 2, 1, 0, 0)
     Timecop.freeze utc
     assign :pursuit, pursuit
     assign :user, user
